2bb2b9d4e569023cb10b09bfebc959499c15b576
bodholtt <tim@bodholtt.im>
2025-01-17 23:51:54 -0500

    Initial

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    optimizeDeps: {
+      exclude: [
+          '@ffmpeg/ffmpeg',
+          '@ffmpeg/util'
+      ],
+    },
+      server: {
+          headers: {
+              "Cross-Origin-Opener-Policy": "same-origin",
+              "Cross-Origin-Embedder-Policy": "require-corp"
+          }
+      }
+  },
+    server: {
+      headers: {
+          "Cross-Origin-Opener-Policy": "same-origin",
+          "Cross-Origin-Embedder-Policy": "require-corp"
+      }
+    }
+});
